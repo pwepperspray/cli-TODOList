d56f1d203db1944b7ba8ac06d6a2ae363a5e03f4
cinnamon_rowll <166984309+pwepperspray@users.noreply.github.com>
2024-09-04 12:36:19 +0530

    Made the data, display in a tabular form

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,83 @@
 #include <iostream>     
+#include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 
-//function definition
+//function declaration
 void list();
 void add();
 void remove();
 void sate();
 void errormsg();
 
-//function explanation
+//function definition
 void list(const std::string &filepath){
-	std::string task;
-	std::cout << "list \n";
-
-	//file operation list
+	//file object
 	std::ifstream file(filepath, std::ios::in);
-	getline(file, task);
-	std::cout << task;
+
+	//Table formatting	
+	std::cout << std::setfill('-') << std::setw(1) << "+" << std::setw(3) << "-" << std::setw(1) << "+" 
+	 	  << std::setw(100) << "-" << std::setw(1) << "+" << std::setw(5) << "-" 
+		  << std::setw(1) << "+" << std::endl; //Table top border
+								 
+	std::cout << std::setfill(' ') << std::setw(1) << "|" << std::setw(3) << std::left << "No."
+		  << std::setw(1) << "|" << std::setw(50) << std::right << "Tasks" << std::setw(51) << std::right << "|"
+		  << std::setw(5) << std::left << "State" << std::setw(1) << "|" << std::endl; //Table column names
+
+	std::cout << std::setfill('-') << std::setw(1) << "+" << std::setw(3) << "-" << std::setw(1) << "+" 
+		  << std::setw(100) << "-" << std::setw(1) << "+" << std::setw(5) << "-" 
+		  << std::setw(1) << "+" << std::endl;//Table low border
+
+
+	std::string line = "";
+	int i = 1;
+	while(getline(file, line)){
+		std::string task;
+		std::string statebuffer;
+		std::string state;
+		std::stringstream inputString(line);
+
+		getline(inputString, task, ',');
+		std::string bufferState = "";
+		getline(inputString, statebuffer,',');
+		state = '[' + statebuffer + ']';
+		//state = atoi(bufferState.c_str());
+		line = "";
+		
+		//displaying the data in a tabular form
+		std::cout << std::setfill(' ') << std::setw(1) << "|" << std::setw(3) << std::left << i << std::setw(1) << "|"
+			  << std::setw(100) << std::left << task << std::setw(1) << std::right << "|" 
+			  << std::setw(5) << std::left << state << std::setw(1) << "|" << std::endl;
+
+		std::cout << std::setfill('-') << std::setw(1) << "+" << std::setw(3) << "-" << std::setw(1) << "+"
+			  << std::setw(100) << "-" << std::setw(1) << "+" << std::setw(5) << "-" 
+			  << std::setw(1) << "+" << std::endl;
+
+		//std::cout << i << " " << task << " " << state << "\n";
+		
+
+
+
+		i++;
+		//std::cout << i << " " << line << "\n";
+	}
 	file.close();
 }
 
 void add(const std::string &filepath, std::string task){
-
 	//file operation add
 	std::ofstream file(filepath, std::ios::app);
-	file << task;
+
+	//writing the task
+	file << task //writing the task
+	     <<"," //delimiter
+	     << " " //writing the state of the task i.e done or not (default is not or -) x == done & - == not
+	     <<"\n";
 	file.close();
-	std::cout << task << "\n";
+
+	std::cout << "Added Task!" << "\n";
 }
 
 void remove(){
@@ -37,12 +87,12 @@ void remove(){
 	std::cout << index;
 }
 
-void sate(){
+void status(){
 	std::cout << "bored \n";
 }
 
 void errormsg(){
-	std::cout << "teri maa bc \n";
+	std::cout << "ERROR! dk what error lazy to write specific error msg maybe later \n";
 }
 
 //main 
@@ -62,9 +112,6 @@ int main (int argc, char* argv[]) {
 		createfile.close();
 	}
 	
-	//initialization of input variables for file
-	std::vector<std::string> task;
-	std::vector<bool> state;
 
 	//initialization of array containing argument variables
 	//inserting the arguments into an array
@@ -81,7 +128,6 @@ int main (int argc, char* argv[]) {
 	//checking the commmand line arguments and working accordingly
 	//list the todo list
 	if(argument[1] == "list"){
-		std::cout << "true \n";
 		list(path);
 	}
 	//add a new task
