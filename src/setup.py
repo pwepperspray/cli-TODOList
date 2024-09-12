492225b8f68752ef4b24fdaf31f2779e047b9cc2
cinnamon_rowll <166984309+pwepperspray@users.noreply.github.com>
2024-09-12 14:42:06 +0530

    Put some ui changes in place

--- src/setup.py
+++ src/setup.py
@@ -9,19 +9,29 @@ def browse_folder():
         folder_contents.delete(0, tk.END)  # Clear the Listbox
         for item in os.listdir(folder_path):
             folder_contents.insert(tk.END, item)  # Insert folder contents into Listbox
-
+    print(folder_path)
+    
 # Create the main window
 root = tk.Tk()
-root.title("File Explorer")
+root.title("cli-TODOList")
 root.geometry("500x250")
 
-top = tk.Label(root, text = "cli-TODOList made by pwepperspray/cinnamon_rowll")
-top.grid(row = 0, column = 5)
+toptext = tk.Label(root, text = "cli-TODOList made by pwepperspray/cinnamon_rowll")
+toptext.place(x = (500/2)-150 ,y = 5)
+
+infotextPositionX = (500/2)-240
+infotext = tk.Label(root, text = "Select the foler you want your data to be stored in :")
+infotext.place(x = (500/2)-240, y = 30) 
+
+
+field = tk.Entry(root)
+field.place(x = (500/2), y = 25)
+
 # Create a button to browse for a folder
 browse_button = tk.Button(root, text="Browse", command=browse_folder)
-browse_button.grid(row = 1, column = 1, pady = 2)
+browse_button.grid(row = 2, column = 5, pady = 2)
 
 # Create a scrollbar for the Listbox
 
-# Start the Tkinter main loop
+# Start the Tkinter main loop'''
 root.mainloop()
